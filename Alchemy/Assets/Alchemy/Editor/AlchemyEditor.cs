d89818a00d9843bc32f1e4f67a3d953408eff330
Yuffter <yuupon2005@gmail.com>
2026-03-03 23:48:22 +0900

    fix: エラー原因特定のためコメントアウト

--- Alchemy/Assets/Alchemy/Editor/AlchemyEditor.cs
+++ Alchemy/Assets/Alchemy/Editor/AlchemyEditor.cs
@@ -108,11 +108,11 @@ namespace Alchemy.Editor
     [CanEditMultipleObjects]
     internal sealed class ScriptableObjectEditor : AlchemyEditor { }
 
-#if ALCHEMY_SUPPORT_NETCODE
-    [CustomEditor(typeof(Unity.Netcode.NetworkBehaviour), editorForChildClasses: true, isFallback = true)]
-    [CanEditMultipleObjects]
-    internal sealed class NetworkBehaviourEditor : AlchemyEditor { }
-#endif
+// #if ALCHEMY_SUPPORT_NETCODE
+//     [CustomEditor(typeof(Unity.Netcode.NetworkBehaviour), editorForChildClasses: true, isFallback = true)]
+//     [CanEditMultipleObjects]
+//     internal sealed class NetworkBehaviourEditor : AlchemyEditor { }
+// #endif
 
 #endif
 
